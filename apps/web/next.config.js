cce5b8be02b50a7ac3d4f8c1ceebc80921fcd505
polaarts <samuel.angulo_c@mail.udp.cl>
2025-07-14 21:13:42 -0400

    fix: eslint problems to build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,12 @@
 const NextConfig = {
   experimental: {
     largePageDataBytes: 1 * 1024 * 1024, // 1mb
+    instrumentationHook: false,
+  },
+  // Disable webpack cache to avoid permission issues
+  webpack: (config) => {
+    config.cache = false;
+    return config;
   },
 };
 
