556b551765c2dfc2a98b36b9adcdcf0dfc2002fa
polaarts <samuel.angulo_c@mail.udp.cl>
2025-07-14 21:20:24 -0400

    fix: reset

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,12 +2,6 @@
 const NextConfig = {
   experimental: {
     largePageDataBytes: 1 * 1024 * 1024, // 1mb
-    instrumentationHook: false,
-  },
-  // Disable webpack cache to avoid permission issues
-  webpack: (config) => {
-    config.cache = false;
-    return config;
   },
 };
 
